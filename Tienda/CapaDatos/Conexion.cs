3a3deca0ef1b4dc910c843e863ca522d5791fff4
AlexUrbano1 <alex.urbano1@outlook.com>
2025-06-19 01:49:23 -0500

    Actualización del software de ventas completo

--- Tienda/CapaDatos/Conexion.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CapaDatos
-{
-    public static class Conexion
-    {
-        public static readonly string connectionString = "Data Source=ANURBANO\\SQLEXPRESS;Initial Catalog=DBSISTEMA_VENTA;Integrated Security=True";
-    }
-}
